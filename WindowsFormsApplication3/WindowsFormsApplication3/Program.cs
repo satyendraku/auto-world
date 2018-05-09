19198a0dcc1f15944217dcd6631708afeb250fe7
Satyendra Kumar Singh <satyendra.k@telxsi.com>
2018-05-09 14:13:20 +0530

    updated program.cs

--- WindowsFormsApplication3/WindowsFormsApplication3/Program.cs
+++ WindowsFormsApplication3/WindowsFormsApplication3/Program.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApplication3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Application.Run(new Form1());
+
         }
     }
 }
